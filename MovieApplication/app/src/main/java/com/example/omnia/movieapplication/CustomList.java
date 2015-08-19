bf462ae583816ed1572e0b323ff620d300198fd3
osmdawy <osmdawy@gmail.com>
2015-08-19 13:30:06 -0700

    Changed from fetching from the web to fetch from the database

--- MovieApplication/app/src/main/java/com/example/omnia/movieapplication/CustomList.java
+++ MovieApplication/app/src/main/java/com/example/omnia/movieapplication/CustomList.java
@@ -1,47 +1,46 @@
 package com.example.omnia.movieapplication;
 
-import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import data.movie.MovieCursor;
 
 /**
  * Created by omnia on 7/22/15.
  */
-public class CustomList extends ArrayAdapter<String> {
-    private final Activity context;
-
-    private  ArrayList<String> imagesUrl;
-    private  ArrayList<String> ids;
-    public CustomList(Activity context, ArrayList<String> imagesUrl) {
-        super(context, R.layout.list_item_film, imagesUrl);
-        this.context = context;
-        this.imagesUrl = imagesUrl;
-        ids = new ArrayList<String>();
-
-    }
+public class CustomList  extends CursorAdapter {
 
 
-    public ArrayList<String> getImagesUrl() {
-        return imagesUrl;
-    }
 
-    public ArrayList<String> getIds() {
-        return ids;
+    public CustomList(Context context, Cursor c, int flags) {
+        super(context, c, flags);
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View rowView= inflater.inflate(R.layout.list_item_film, null, true);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.one_film_image);
-        Picasso.with(context).load(imagesUrl.get(position)).into(imageView);
-        return rowView;
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_film, parent, false);
+
+        return view;
+    }
+
+
+    /*
+        This is where we fill-in the views with the contents of the cursor.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // our view is pretty simple here --- just a text view
+        // we'll keep the UI functional with a simple (and slow!) binding.
+        ImageView imageView = (ImageView) view.findViewById(R.id.one_film_image);
+
+        Picasso.with(context).load(((MovieCursor)cursor).getPosterUrl()).into(imageView);
+
     }
 }
--- MovieApplication/app/src/main/java/com/example/omnia/movieapplication/FilmDetailActivity.java
+++ MovieApplication/app/src/main/java/com/example/omnia/movieapplication/FilmDetailActivity.java
@@ -1,11 +1,8 @@
 package com.example.omnia.movieapplication;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -14,16 +11,8 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import data.movie.MovieCursor;
+import data.movie.MovieSelection;
 
 
 public class FilmDetailActivity extends ActionBarActivity {
@@ -34,12 +23,26 @@ public class FilmDetailActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_detail);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(getString(R.string.film_id));
-
-        new FetchFilmTask().execute(message);
-
+        Long id = intent.getLongExtra(getString(R.string.film_id), 1);
+        String[] result = parseResultFromCursor(id);
+        changeView(result);
     }
-
+    public String[] parseResultFromCursor(long movieId){
+        MovieSelection where = new MovieSelection();
+        where.id(movieId);
+        MovieCursor cursor = where.query(getContentResolver());
+        cursor.moveToFirst();
+        String [] result = new String[5];
+        if(cursor!=null) {
+            result[0] = cursor.getTitle();
+            result[1] = cursor.getPlotSynopsis();
+            result[2] = cursor.getPosterUrl();
+            result[3] = cursor.getVoteAverage() + "";
+            result[4] = cursor.getReleaseDate();
+        }
+        cursor.close();
+        return  result;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -75,76 +78,5 @@ public class FilmDetailActivity extends ActionBarActivity {
         TextView release_date = (TextView)findViewById(R.id.film_release_date);
         release_date.setText(result[4]);
     }
-    private class FetchFilmTask extends AsyncTask<String, Void, String[]>{
 
-        @Override
-        protected String[] doInBackground(String... params) {
-            String id = params[0];
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            try {
-                Uri.Builder builder = new Uri.Builder();
-                builder.encodedPath("http://api.themoviedb.org/3/movie/");
-                builder.appendEncodedPath(id);
-                builder.appendQueryParameter("api_key",AllFilmsActivityFragment.API_KEY);
-                URL url = new URL(builder.toString());
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                String oneMovie = buffer.toString();
-                return getFilmDetails(oneMovie);
-
-            }catch (MalformedURLException e){
-                Log.e("MalformedURLException", e.getMessage());
-            }catch (IOException e){
-                Log.e("IOException",e.getMessage());
-            }catch (JSONException e){
-                Log.e("JSONException",e.getMessage());
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String[] result) {
-            super.onPostExecute(result);
-            changeView(result);
-
-        }
-
-        public String[] getFilmDetails(String jsonStr) throws JSONException {
-            String [] result = new String [5];
-            final String FILM_IMAGE = "poster_path";
-            JSONObject filmJson = new JSONObject(jsonStr);
-            result[0] = filmJson.getString("original_title");
-            result[1] = filmJson.getString("overview");
-            result[2] = "http://image.tmdb.org/t/p/w185/"+filmJson.getString(FILM_IMAGE);
-            result[3] = filmJson.getString("vote_average");
-            result[4] = filmJson.getString("release_date");
-            return result;
-        }
-    }
 }
